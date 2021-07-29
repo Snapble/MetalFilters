18853d931a0592b32bb89c0d7004b1b63daa51d7
hai <haithngn@gmail.com>
2021-07-29 22:11:32 +0700

    - enable carthage

---
+++ MetalFilters/MetalFilters.h
@@ -0,0 +1,19 @@
+//
+//  MetalFilters.h
+//  MetalFilters
+//
+//  Created by Hai Nguyen on 7/29/21.
+//  Copyright © 2021 shuifeng.me. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MetalFilters.
+FOUNDATION_EXPORT double MetalFiltersVersionNumber;
+
+//! Project version string for MetalFilters.
+FOUNDATION_EXPORT const unsigned char MetalFiltersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetalFilters/PublicHeader.h>
+
+
